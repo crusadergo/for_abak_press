2906807a8713893a3ba9757ec3983a837b4251ca
CrusaderGo <Crusadergo@yandex.ru>
2014-07-24 22:21:24 +0600

    before_validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,14 +1,15 @@
 class Page < ActiveRecord::Base
   belongs_to :parent, class_name: 'Page'
   has_many :subpages, foreign_key: 'parent_id', class_name: 'Page'
-  validates :url, uniqueness: true
+
+  validates :url, presence: true, uniqueness: true
   validates :name, :body, :title, presence: true
-  def url
-    _url = "/#{name}"
-    if parent.nil?
-      _url
-    else
-      parent.url + _url
+
+  before_validation do
+    self.url = "/#{name.downcase}"
+
+    unless parent.nil?
+      self.url = parent.url + self.url
     end
   end
 
