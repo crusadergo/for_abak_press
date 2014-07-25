f14d2d65e3756ae690eb787641ebf417e2551870
CrusaderGo <Crusadergo@yandex.ru>
2014-07-25 22:45:48 +0600

    Page model refactioring

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
   belongs_to :parent, class_name: 'Page'
-  has_many :subpages, foreign_key: 'parent_id', class_name: 'Page'
+  has_many :children, foreign_key: :parent_id, class_name: 'Page'
 
   validates :url, presence: true, uniqueness: true
   validates :name, :body, :title, presence: true
@@ -11,10 +11,6 @@ class Page < ActiveRecord::Base
     unless parent.nil?
       self.url = parent.url + self.url
     end
-  end
-
-  def children
-    Page.where parent: self
   end
 
 end
